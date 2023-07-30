8421fe902f731a9dbaf32ed1aebe6c1c08d5dc14
NathaliaPino <101751308+NathaliaPino@users.noreply.github.com>
2023-07-30 16:39:21 -0300

    #

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
     /*const playButton = document.getElementById("playButton");*/
-    const playButton = document.querySelector(".playButton")
-    const videoOverlay = document.getElementById("videoOverlay");
-    const closeButton = document.getElementById("closeButton");
-    const videoPlayer = document.getElementById("videoPlayer");
+    const botaoPlay = document.querySelector(".botaoPlay")
+    const videoId = document.getElementById("videoId");
+    const botaoFechar = document.getElementById("botaoFechar");
+    const video = document.getElementById("video");
+
+    const botaoPlay2 = document.querySelector(".play-circle")
 
     
 
     // Função para mostrar a camada do vídeo e reproduzir o vídeo
-    function showVideoOverlay() {
-        videoOverlay.style.display = "flex";
-        videoPlayer.play();
+    function mostrarVideo() {
+        videoId.style.display = "flex";
+        video.play();
     }
 
      // Função para ocultar a camada do vídeo e pausar o vídeo
-    function hideVideoOverlay() {
-        videoOverlay.style.display = "none";
+    function ocultarVideo() {
+        videoId.style.display = "none";
           // Pausar o vídeo ao fechar a camada
-        videoPlayer.pause();
+          video.pause();
     }
 
     // Event listener para o clique no botão play
-    playButton.addEventListener("click", showVideoOverlay);
+    botaoPlay.addEventListener("click", mostrarVideo);
+
+    botaoPlay2.addEventListener("click", mostrarVideo)
+
 
     // Event listener para o clique no botão de fechar
-    closeButton.addEventListener("click", hideVideoOverlay);
+    botaoFechar.addEventListener("click", ocultarVideo);
